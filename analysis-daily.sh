e96bf7c29846464af7a6af5a2e0a473603fc9fd3
Z <xzblogin@163.com>
2024-07-09 13:45:41 +0800

    Update analysis-daily.sh

--- analysis-daily.sh
+++ analysis-daily.sh
@@ -7,7 +7,7 @@ name="`ls -l|wc -l`_`date | awk -F " " '{print $6"-"$2"-"$3}'`"
 touch "$name".txt                                                                                                                                      
 cat /var/log/openvpn.log | awk -F " " '{print $1" "$2" "$3" "$4" "$5" "$6}' | grep "`date |awk -F " " '{print $1" "$2" "$3}'`" | awk '!a[$0]++{print}' 
 > /var/log/open-login/login-date-log/"$name".txt                                                                                                       
-sshpass -pElephant123 scp -r /var/log/open-login/login-date-log/"$name".txt root@192.168.1.11:/Elephant_backup/Open_VPN-log/  
+sshpass -ppassword scp -r /var/log/open-login/login-date-log/"$name".txt root@192.168.1.11:/my_backup/Open_VPN-log/  
 
 
 
